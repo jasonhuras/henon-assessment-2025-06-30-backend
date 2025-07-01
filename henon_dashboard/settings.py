00a1fb639dc40849e17a7085bc86577fa19df856
Jason Huras <hurasj@uoguelph.ca>
2025-07-01 11:44:12 -0400

    cleanup

--- henon_dashboard/settings.py
+++ henon_dashboard/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-j4f+k89844zr1p8sei^h-!xd0s+&px!f@c4e-s+vi*qe^xs_k5"
+SECRET_KEY = os.getenv("DJANGO_SECURITY_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = []
 
