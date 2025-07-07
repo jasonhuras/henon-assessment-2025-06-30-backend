abe826ad2247e18cbce59b7408247c59921940d9
Jason Huras <hurasj@uoguelph.ca>
2025-07-07 19:59:38 -0400

    Update middleware.py

--- currencies/middleware.py
+++ currencies/middleware.py
@@ -1,12 +1,12 @@
 from django.http import JsonResponse
 from django.conf import settings
-
+from django.utils.crypto import constant_time_compare
 
 class APIKeyMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    # middleware.py
+    
     def __call__(self, request):
         if request.method == "OPTIONS":
             return self.get_response(request)
@@ -14,7 +14,7 @@ class APIKeyMiddleware:
         if request.path.startswith("/api/"):
             api_key = request.headers.get("X-API-Key")
 
-            if api_key != settings.API_KEY:
+            if not api_key or not constant_time_compare(api_key, settings.API_KEY):
                 return JsonResponse({"error": "Invalid API key"}, status=401)
 
         return self.get_response(request)
